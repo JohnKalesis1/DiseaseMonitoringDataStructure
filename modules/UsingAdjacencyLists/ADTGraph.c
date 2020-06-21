eed7d4b6dd0f3ed3825fd561d7c1b9285bb79219
JohnKalesis1 <sdi1900069@di.uoa.gr>
2020-06-21 15:04:40 +0300

    done

--- modules/UsingAdjacencyLists/ADTGraph.c
+++ modules/UsingAdjacencyLists/ADTGraph.c
@@ -7,17 +7,17 @@
 
 struct graph {
     Map map;
-    int size;
+    int size; 
     CompareFunc compare;
     DestroyFunc destroy;
     HashFunc hash_function;
 };
 struct weighted_vertex  {
-    Pointer vertex;
+    Pointer vertex;   //δομη που αποθηκευει το βαρος της ακμης του vertex 
     uint weight;
 };
 struct distance_vertex  {
-    Pointer vertex;
+    Pointer vertex;  //ακριβως το ιδιο πραγμα (για χρηση στο Dijkstra) απλα για να ειναι πιο κατανοητο εχει διαφορετικη ονομασια
     uint distance;
 };
 typedef struct distance_vertex * D_Vertex;
@@ -52,7 +52,7 @@ int graph_size(Graph graph)  {
 }
 
 void graph_insert_vertex(Graph graph, Pointer vertex)  {
-    List list=list_create(free);
+    List list=list_create(free);  //στο map αντιστοιχουμε καθε vertex(key) με μια λιστα που περιεχει δομες weighted_vertex 
     map_insert(graph->map,vertex,list);
     graph->size++;
 }
@@ -61,7 +61,7 @@ List graph_get_vertices(Graph graph)  {
     List list=list_create(NULL);
     MapNode node=map_first(graph->map);
     if (graph->size!=0)  {
-        while (node!=MAP_EOF)  {
+        while (node!=MAP_EOF)  {  //διατρεχουμε το map για να παρουμε ολα τα vertex (δλδ τα κλειδια του)
             list_insert_next(list,LIST_BOF,map_node_key(graph->map,node));
             node=map_next(graph->map,node);
         }
@@ -70,10 +70,10 @@ List graph_get_vertices(Graph graph)  {
 }
 
 void graph_remove_vertex(Graph graph, Pointer vertex)  {
-    List list=map_node_value(graph->map,map_find_node(graph->map,vertex));
-    ListNode lnode=list_first(list);
+    List list=map_node_value(graph->map,map_find_node(graph->map,vertex));  //αφαιρουμε καθε υπαρξη του vertex στους γειτονες του
+    ListNode lnode=list_first(list);  //δλδ για καθε γειτονα του vertex αφαιρουμε την δομη weighted_vertex που αντιστοιχει στο vertex μας
     if (list_size(list)!=0)  {
-        Pointer adj_vertex=((W_Vertex)list_node_value(list,lnode))->vertex;
+        Pointer adj_vertex=((W_Vertex)list_node_value(list,lnode))->vertex;  
         graph_remove_edge(graph,vertex,adj_vertex);
     }
     while(list_size(list)!=0)  {
@@ -81,30 +81,28 @@ void graph_remove_vertex(Graph graph, Pointer vertex)  {
         graph_remove_edge(graph,vertex,adj_vertex);
         lnode=list_next(list,lnode);
     }
-    map_remove(graph->map,vertex);
+    map_remove(graph->map,vertex);  //και τελος διαγραφουμε το κλειδι μας μαζι με τη λιστα γειτονων του 
     graph->size--;
     return ;
 }
 
 void graph_insert_edge(Graph graph, Pointer vertex1, Pointer vertex2, uint weight)  {
-    MapNode m_node1=map_find_node(graph->map,vertex1);
+    MapNode m_node1=map_find_node(graph->map,vertex1);  //βρισκουμε τους κομβους του map που αντιστοιχουν στα κλειδια μας 
     MapNode m_node2=map_find_node(graph->map,vertex2);
     List list;
-    list=(List)map_node_value(graph->map,m_node1);
+    list=(List)map_node_value(graph->map,m_node1);   //και προσθετουμε στις λιστες γειτνιασης τους την ακμη που τα συνδεει
     list_insert_next(list,LIST_BOF,create_edge(vertex2,weight));
-    //map_insert(graph->map,vertex1,list);
     list=(List)map_node_value(graph->map,m_node2);
     list_insert_next(list,LIST_BOF,create_edge(vertex1,weight));
-    //map_insert(graph->map,vertex2,list);
     return ;
 }
 
 void graph_remove_edge(Graph graph, Pointer vertex1, Pointer vertex2)  {
-    MapNode m_node1=map_find_node(graph->map,vertex1);
+    MapNode m_node1=map_find_node(graph->map,vertex1); //βρισκουμε τους κομβους του map που αντιστοιχουν στα κλειδια μας 
     MapNode m_node2=map_find_node(graph->map,vertex2);
     List list;
     ListNode lnode;
-    list=(List)map_node_value(graph->map,m_node1);
+    list=(List)map_node_value(graph->map,m_node1);  //και διατρεχουμε τις λιστες τους εως να βρουμε την αντιστοιχη δομη weighted_vertex και να τη διαγραψουμε
     lnode=list_first(list);
     if (graph->compare(((W_Vertex)list_node_value(list,lnode))->vertex,vertex2)==0)  {
         list_remove_next(list,LIST_BOF);
@@ -115,7 +113,6 @@ void graph_remove_edge(Graph graph, Pointer vertex1, Pointer vertex2)  {
         }
         list_remove_next(list,lnode);
     }
-    //map_insert(graph->map,vertex1,list);
     list=(List)map_node_value(graph->map,m_node2);
     lnode=list_first(list);
     if (graph->compare(((W_Vertex)list_node_value(list,lnode))->vertex,vertex1)==0)  {
@@ -127,13 +124,12 @@ void graph_remove_edge(Graph graph, Pointer vertex1, Pointer vertex2)  {
         }
         list_remove_next(list,lnode);
     }
-    //map_insert(graph->map,vertex2,list);
     return ;
 }
 
 uint graph_get_weight(Graph graph, Pointer vertex1, Pointer vertex2)  {
-    MapNode m_node=map_find_node(graph->map,vertex1);
-    List list=(List)map_node_value(graph->map,m_node);
+    MapNode m_node=map_find_node(graph->map,vertex1); //βρισκουμε το κομβο του κλειδιου μας και διατρεχουμε τη λιστα γειτνιασης του 
+    List list=(List)map_node_value(graph->map,m_node);//εως να βρουμε την αντιστοιχη δομη την οποια και επιστρεφουμε το βαρος της
     ListNode lnode=list_first(list);
     if (lnode!=LIST_BOF)  {
         while (lnode!=LIST_EOF && graph->compare(((W_Vertex)list_node_value(list,lnode))->vertex,vertex2)!=0)  {
@@ -143,19 +139,19 @@ uint graph_get_weight(Graph graph, Pointer vertex1, Pointer vertex2)  {
             return ((W_Vertex)list_node_value(list,lnode))->weight;
         }
         else  {
-            return UINT_MAX;
+            return UINT_MAX;  // αν δεν βρεθει επιστρεφουμε απειρο
         }
     }
-    return UINT_MAX;
+    return UINT_MAX; //αν δεν εχει γειτονες επιστρεφουμε απειρο
 }
 
 
 List graph_get_adjacent(Graph graph, Pointer vertex)  {
     List list,r_list=list_create(NULL);
-    MapNode m_node=map_find_node(graph->map,vertex);
+    MapNode m_node=map_find_node(graph->map,vertex); //βρισκουμε τον κομβο που αντιστοιχει στο κλειδι μας
     list=(List)map_node_value(graph->map,m_node);
     ListNode lnode=list_first(list);
-    while (lnode!=LIST_EOF)  {
+    while (lnode!=LIST_EOF)  { //αντιγραφουμε τα vertex απο τις δομες weighted_vertex της λιστας γειτνιασης στη λιστα που θα επιστρεψουμε 
         list_insert_next(r_list,LIST_BOF,((W_Vertex)list_node_value(list,lnode))->vertex);
         lnode=list_next(list,lnode);
     }
@@ -176,45 +172,41 @@ int* create_int(int value)  {
 }
 
 int compare_distances(Pointer a,Pointer b)  {
-    D_Vertex vertex1= (D_Vertex) a;
-    D_Vertex vertex2= (D_Vertex) b;
-    return (-1)*(vertex1->distance-vertex2->distance);
+    D_Vertex vertex1= (D_Vertex) a;  
+    D_Vertex vertex2= (D_Vertex) b;   
+    return (-1)*(vertex1->distance-vertex2->distance);  //μας νοιαζει να επιλεγουμε πρωτα τις μικροτερες αποστασεις 
 }
 int compare_bool(Pointer a,Pointer b)  {
     return (*(bool*)a-*(bool*)b);
 }
-int unimportant(Pointer a,Pointer b)  {
-    return 0;
-}
 List graph_shortest_path(Graph graph, Pointer source, Pointer target)  {
-    Map spider_web=map_create(compare_bool,NULL,free);
-    Map pqueue_vertex_relate=map_create(graph->compare,NULL,NULL);
-    PriorityQueue distance_array=pqueue_create(compare_distances,NULL,NULL);
-    Map previous_shortest=map_create(unimportant,NULL,NULL);
+    Map spider_web=map_create(graph->compare,NULL,free); //χαρτης που αντιστοιχει καθε vertex σε true ή false τιμες , αν αντιστοιχα εχει επιλεγει μονοπατι εως το στοιχειο ή οχι
+    Map pqueue_vertex_relate=map_create(graph->compare,NULL,NULL); //με στοχο να χρησιμποποιησουμε την pq_update_order η οποια παιρνει ορισμα PriorityQueueNode , πρεπει για καθε vertex να ξερουμε σε ποιο κομβο ουραςς αντιστοιχει
+    PriorityQueue distance_array=pqueue_create(compare_distances,NULL,NULL);  //ουρα προταιρεοτητας για την επιλογη των μονοπατιων με την ελαχιστη αποσταση
+    Map previous_shortest=map_create(graph->compare,NULL,NULL); //αντιστοιχηση καθε vertex με αυτον τον οποιο ειναι το καλυτερο μονοπατι για να φτασουμε στο vertex μας 
     map_set_hash_function(spider_web,graph->hash_function);
     map_set_hash_function(pqueue_vertex_relate,graph->hash_function);
     map_set_hash_function(previous_shortest,graph->hash_function);
     MapNode mnode=map_first(graph->map);
     List r_list=list_create(NULL);
     for (int i=0;i<graph->size;i++)  {
-        map_insert(spider_web,map_node_key(graph->map,mnode),create_bool(0));
+        map_insert(spider_web,map_node_key(graph->map,mnode),create_bool(0)); //αρχικα δεν εχουμε επιλεξει κανενα vertex 
         mnode=map_next(graph->map,mnode);
     }
     map_insert(previous_shortest,source,NULL);
-    //map_insert(spider_web,source,create_bool(1));
-    PriorityQueueNode qnode=pqueue_insert(distance_array,create_distance(source,0));
+    PriorityQueueNode qnode=pqueue_insert(distance_array,create_distance(source,0)); //και η μονη επιλογη μας ειναι να παμε στο source (το 0 δε παιζιε ρολο αφου θα διαγραφει μετα , συμβολικα το βαλα)
     map_insert(pqueue_vertex_relate,source,qnode);
     D_Vertex d_vertex;
     while(pqueue_size(distance_array)!=0)  {
-        d_vertex=pqueue_max(distance_array);
+        d_vertex=pqueue_max(distance_array);  //απο τα διαθεσιμα μονοπατια επιλεγουμε αυτο με την μικροτερη αποσταση
         pqueue_remove_max(distance_array);
-        if (*(bool*)map_node_value(spider_web,map_find_node(spider_web,d_vertex->vertex))==true)  {
+        if (*(bool*)map_node_value(spider_web,map_find_node(spider_web,d_vertex->vertex))==true)  {  //αν εχουμε ξαναπερασει απο το vertex τοτε επιλεγουμε αλλο μονοπατι
             free(d_vertex);
             continue ;
         }
-        if (graph->compare(d_vertex->vertex,target)==0)  {
+        if (graph->compare(d_vertex->vertex,target)==0)  { //αν φτασουμε στοπ προορισμο μας σημαινει οτι βρικαμε το καλυτερο μονοπατι για αυτον
             if (map_node_value(previous_shortest,map_find_node(previous_shortest,target))!=NULL)  {
-                list_insert_next(r_list,LIST_BOF,target);
+                list_insert_next(r_list,LIST_BOF,target); //και επιστρεφουμε τη λιστα με τους προηγουμενους vertex της καλυτερης διαδρομης
                 Pointer vertex=target;
                 while ((vertex=map_node_value(previous_shortest,map_find_node(previous_shortest,vertex)))!=NULL)  {
                     list_insert_next(r_list,LIST_BOF,vertex);
@@ -228,27 +220,27 @@ List graph_shortest_path(Graph graph, Pointer source, Pointer target)  {
             pqueue_destroy(distance_array);
             return r_list;
         }
-        map_insert(spider_web,d_vertex->vertex,create_int(1));
+        map_insert(spider_web,d_vertex->vertex,create_int(1)); // αλλιως σηματοδοτουμε οτι πια εχουμε περασει απο το vertex αυτο
         List list=map_node_value(graph->map,map_find_node(graph->map,d_vertex->vertex));
-        ListNode lnode=list_first(list);
+        ListNode lnode=list_first(list); //και τωρα κοιταμε τους γειτονες του 
         while (lnode!=LIST_EOF)  {
             if (*(bool*)map_node_value(spider_web,map_find_node(spider_web,((W_Vertex)list_node_value(list,lnode))->vertex))==1)  {
-                lnode=list_next(list,lnode);
+                lnode=list_next(list,lnode); //αν εχουμε ηδη φτασει σε εναν απο τους γειτονες παμε στον επομενο
                 continue ;
             }
-            uint alt_route=d_vertex->distance+((W_Vertex)list_node_value(list,lnode))->weight;
-            if (map_find_node(pqueue_vertex_relate,((W_Vertex) list_node_value(list,lnode))->vertex)!=MAP_EOF)  {
+            uint alt_route=d_vertex->distance+((W_Vertex)list_node_value(list,lnode))->weight; // για εναν γειτονα που δεν εχουμε φτασει υπαρχουν δυο πιθανοτητες
+            if (map_find_node(pqueue_vertex_relate,((W_Vertex) list_node_value(list,lnode))->vertex)!=MAP_EOF)  {//να εχουμε ηδη υπολογισει την αποσταση που χρειαζεται για να παμε σε αυτον
                 qnode=map_node_value(pqueue_vertex_relate,map_find_node(pqueue_vertex_relate,((W_Vertex) list_node_value(list,lnode))->vertex));
                 uint d=((D_Vertex)pqueue_node_value(distance_array,qnode))->distance;
-                if (alt_route<d)  {
+                if (alt_route<d)  { //και αν η νεα αποσταση για να φτασουμε στον κομβο ειναι καλυτερη απο την παλια την αντικαθισταμε
                     ((D_Vertex)pqueue_node_value(distance_array,qnode))->distance=alt_route;
                     pqueue_update_order(distance_array,qnode);
                     map_insert(previous_shortest,((W_Vertex)list_node_value(list,lnode))->vertex,d_vertex->vertex);
                 }
             }
-            else  {
+            else  { //αλλιως ειναι η πρωτη φορα που υπολογιζουμε την αποσταση σε αυτον τον κομβο 
                 qnode=pqueue_insert(distance_array,create_distance(((W_Vertex)list_node_value(list,lnode))->vertex,d_vertex->distance+((W_Vertex)list_node_value(list,lnode))->weight));
-                map_insert(pqueue_vertex_relate,((W_Vertex)list_node_value(list,lnode))->vertex,qnode);
+                map_insert(pqueue_vertex_relate,((W_Vertex)list_node_value(list,lnode))->vertex,qnode); //και την βαζουμε ως μια απο τις επιλογες στην ουρα 
                 map_insert(previous_shortest,((W_Vertex)list_node_value(list,lnode))->vertex,d_vertex->vertex);
             }
             lnode=list_next(list,lnode);   
@@ -260,7 +252,8 @@ List graph_shortest_path(Graph graph, Pointer source, Pointer target)  {
     map_destroy(previous_shortest);
     map_destroy(pqueue_vertex_relate);
     pqueue_destroy(distance_array);
-    return r_list; 
+    return r_list;  //αν φτασουμε εδω σημαινει οτι εξαντλησαμε ολα τα μονοπατια απο την αφετηρια μας και δεν βρηκαμε καποιο που να φτανει στο προορισμο μας 
+                    //οποτε επιστρεφουμε μια κενη λιστα 
 }
 
 void graph_destroy(Graph graph)  {
--- modules/UsingHashTable/ADTMap.c
+++ modules/UsingHashTable/ADTMap.c
@@ -1,53 +1,35 @@
-/////////////////////////////////////////////////////////////////////////////
-//
-// Υλοποίηση του ADT Map μέσω Hash Table με open addressing (linear probing)
-//
-/////////////////////////////////////////////////////////////////////////////
-
 #include <stdlib.h>
-
 #include "ADTMap.h"
 
-
-
-
-
-// Το μέγεθος του Hash Table ιδανικά θέλουμε να είναι πρώτος αριθμός σύμφωνα με την θεωρία.
-// Η παρακάτω λίστα περιέχει πρώτους οι οποίοι έχουν αποδεδιγμένα καλή συμπεριφορά ως μεγέθη.
-// Κάθε re-hash θα γίνεται βάσει αυτής της λίστας. Αν χρειάζονται παραπάνω απο 1610612741 στοχεία, τότε σε καθε rehash διπλασιάζουμε το μέγεθος.
 int prime_sizes[] = {53,  97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157, 98317, 196613, 393241,
 	786433, 1572869, 3145739, 6291469, 12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741};
 
-// Χρησιμοποιούμε open addressing, οπότε σύμφωνα με την θεωρία, πρέπει πάντα να διατηρούμε
-// τον load factor του  hash table μικρότερο ή ίσο του 0.5, για να έχουμε αποδoτικές πράξεις
+
 #define MAX_LOAD_FACTOR 0.9
 
-// Δομή του κάθε κόμβου που έχει το hash table (με το οποίο υλοιποιούμε το map)
 struct map_node{
-	Pointer key;		// Το κλειδί που χρησιμοποιείται για να hash-αρουμε
-	Pointer value;  	// Η τιμή που αντισοιχίζεται στο παραπάνω κλειδί
-	MapNode next;	// Μεταβλητή για να μαρκάρουμε την κατάσταση των κόμβων (βλέπε διαγραφή)
-	bool state;
+	Pointer key;		
+	Pointer value;  	
+	MapNode next;	
+	bool state; // false για αδεια λιστα,true για λιστα που εχει στοιχεια 
 };
 
-// Δομή του Map (περιέχει όλες τις πληροφορίες που χρεαζόμαστε για το HashTable)
+
 struct map {
-	MapNode array;				// Ο πίνακας που θα χρησιμοποιήσουμε για το map (remember, φτιάχνουμε ένα hash table)
-	int capacity;				// Πόσο χώρο έχουμε δεσμεύσει.
-	int size;					// Πόσα στοιχεία έχουμε προσθέσει
-	CompareFunc compare;		// Συνάρτηση για σύγκρηση δεικτών, που πρέπει να δίνεται απο τον χρήστη
-	HashFunc hash_function;		// Συνάρτηση για να παίρνουμε το hash code του κάθε αντικειμένου.
-	DestroyFunc destroy_key;	// Συναρτήσεις που καλούνται όταν διαγράφουμε έναν κόμβο απο το map.
+	MapNode array;				
+	int capacity;				
+	int size;					
+	CompareFunc compare;		
+	HashFunc hash_function;		
+	DestroyFunc destroy_key;	
 	DestroyFunc destroy_value;
 };
 
 Map map_create(CompareFunc compare, DestroyFunc destroy_key, DestroyFunc destroy_value) {
-	// Δεσμεύουμε κατάλληλα τον χώρο που χρειαζόμαστε για το hash table
 	Map map = malloc(sizeof(*map));
 	map->capacity = prime_sizes[0];
 	map->array = malloc(map->capacity * sizeof(struct map_node));
 
-	// Αρχικοποιούμε τους κόμβους που έχουμε σαν διαθέσιμους.
 	for (int i = 0; i < map->capacity; i++)
 		map->array[i].state=0;
 
@@ -59,73 +41,63 @@ Map map_create(CompareFunc compare, DestroyFunc destroy_key, DestroyFunc destroy
 	return map;
 }
 
-// Επιστρέφει τον αριθμό των entries του map σε μία χρονική στιγμή.
 int map_size(Map map) {
 	return map->size;
 }
 
-// Συνάρτηση για την επέκταση του Hash Table σε περίπτωση που ο load factor μεγαλώσει πολύ.
 static void rehash(Map map) {
-	// Αποθήκευση των παλιών δεδομένων
 	int old_capacity = map->capacity;
 	MapNode old_array = map->array;
 
-	// Βρίσκουμε τη νέα χωρητικότητα, διασχίζοντας τη λίστα των πρώτων ώστε να βρούμε τον επόμενο. 
-	int prime_no = sizeof(prime_sizes) / sizeof(int);	// το μέγεθος του πίνακα
-	for (int i = 0; i < prime_no; i++) {					// LCOV_EXCL_LINE
+	int prime_no = sizeof(prime_sizes) / sizeof(int);	
+	for (int i = 0; i < prime_no; i++) {					
 		if (prime_sizes[i] > old_capacity) {
 			map->capacity = prime_sizes[i]; 
 			break;
 		}
 	}
-	// Αν έχουμε εξαντλήσει όλους τους πρώτους, διπλασιάζουμε
-	if (map->capacity == old_capacity)					// LCOV_EXCL_LINE
-		map->capacity *= 2;								// LCOV_EXCL_LINE
+	if (map->capacity == old_capacity)					
+		map->capacity *= 2;								
 
-	// Δημιουργούμε ένα μεγαλύτερο hash table
 	map->array = malloc(map->capacity * sizeof(struct map_node));
 	for (int i = 0; i < map->capacity; i++)
 		map->array[i].state=0;
 
-	map->size=0;
+	map->size=0;  //εφοσον εχουμε αυξησει το μεγεθος του map , αντιγραφουμε τα στοιχεια
 	for (int i = 0; i < old_capacity; i++)  {
 		MapNode temp,node=&old_array[i];
 		bool first=1;
 		if (old_array[i].state==1)  {
 			while (node!=NULL)  {
-				map_insert(map,node->key,node->value);
+				map_insert(map,node->key,node->value); //insert στο νεο map
 				temp=node;
 				node=node->next;
-				if (first!=1)  {
-					free(temp);
-				}
+				if (first!=1)  { 
+					free(temp); //δε κανουμε free το πρωτο στοιχειο της λιστας , διοτι τα πρωτα στοιχεια καθε hash
+				}				//θα γινουν free απο τη map_destrοy
 				first=0;
 			}
 		}
 	}
-	//Αποδεσμεύουμε τον παλιό πίνακα ώστε να μήν έχουμε leaks
 	free(old_array);
 }
 
-// Εισαγωγή στο hash table του ζευγαριού (key, item). Αν το key υπάρχει,
-// ανανέωση του με ένα νέο value, και η συνάρτηση επιστρέφει true.
+
 
 void map_insert(Map map, Pointer key, Pointer value) {
-	// Σκανάρουμε το Hash Table μέχρι να βρούμε διαθέσιμη θέση για να τοποθετήσουμε το ζευγάρι,
-	// ή μέχρι να βρούμε το κλειδί ώστε να το αντικαταστήσουμε.
 	bool already_in_map = false;
 	uint pos=map->hash_function(key) % map->capacity;
 	MapNode node = &(map->array[pos]);
-	if (node->state==1)  {
-		while (node->next!=NULL)  {
+	if (node->state==1)  { //σημαινει οτι υπαρχουν ηδη αλλα στοιχεια στο hash
+		while (node->next!=NULL)  { //διασχιζουμε τη "λιστα" κια τοποθετουμε στο τελος το στοιχειο  
 			if (map->compare(key,node->key)==0)  {
-				already_in_map=true;
+				already_in_map=true; // σημαια οτι υπαρχει ισοδυναμο κλειδι
 				break;
 			}
 			node=node->next;
 		}
 		if (map->compare(key,node->key)==0)  {
-			already_in_map=true;
+			already_in_map=true;  //περιπτωση να ειναι το τελευταιο
 		}
 		if (already_in_map==true)  {
 			if (map->destroy_value!=NULL)  {
@@ -134,50 +106,48 @@ void map_insert(Map map, Pointer key, Pointer value) {
 			if (map->destroy_key!=NULL)  {
 				map->destroy_key(node->key);
 			}
-			node->key=key;
-			node->value = value;
+			node->key=key;  //αντικατασταση 
+			node->value = value; 
 		}
-		else  {
-			map->size++;
+		else  { //προσθεση στο τελος 
+			map->size++;   
 			node->next=malloc(sizeof(struct map_node));
-			node->next->state=1;
+			node->next->state=1; // καθε κομβος με στοιχειο πρεπει να εχει κατασταση true για δουλεψει η remove
 			node->next->key= key;
 			node->next->next= NULL;
 			node->next->value = value;
 		}
 	}
 	else  {
-		map->size++;
-		node->state=1;
+		map->size++;  
+		node->state=1; // τοποθετουμε το πρωτο στοιχειο στη κενη "λιστα"
 		node->key = key;
 		node->next= NULL;
 		node->value = value;
 
 	}
 	
-	// Αν με την νέα εισαγωγή ξεπερνάμε το μέγιστο load factor, πρέπει να κάνουμε rehash
 	float load_factor = (float)map->size / map->capacity;
 	if (load_factor > MAX_LOAD_FACTOR)
 		rehash(map);
 }
 
-// Διαργραφή απο το Hash Table του κλειδιού με τιμή key
 bool map_remove(Map map, Pointer key) {
 	if (map_find(map,key)!=MAP_EOF)  {
 		uint pos= map->hash_function(key) % map->capacity;
 		MapNode prev_node=NULL,node=&(map->array[pos]);
 		while (node!=NULL)  {
-			if (map->compare(node->key,key)==0)  {
-				map->size--;
+			if (map->compare(node->key,key)==0)  {  //αν το βορυμε το διαγραφουμε
+				map->size--;          
 				if (prev_node!=NULL)  {
-					prev_node->next=node->next;
+					prev_node->next=node->next;  //συνδεση του προηγουμενου κομβου με τον επομενο 
 				}
-				else  {
-					if (node->next!=NULL)  {
-						map->array[pos]=*(node->next);
+				else  {  //το στοιχειο που θα αφαιρεσουμε εινια το πρωτο στοιχειο
+					if (node->next!=NULL)  { //αν εχει επομενο 
+						map->array[pos]=*(node->next);  //συνδεουμε την αφετηρια της "λιστας" με το επομενο
 					}
 					else  {
-						map->array[pos].state=0;
+						map->array[pos].state=0; //αλλιως σηματοδοτουμε οτι δεν υπαρχουν στοιχεια στο hash 
 					}
 				}
 				if (map->destroy_key != NULL)
@@ -197,7 +167,6 @@ bool map_remove(Map map, Pointer key) {
 	return false;
 }
 
-// Αναζήτηση στο map, με σκοπό να επιστραφεί το value του κλειδιού που περνάμε σαν όρισμα.
 
 Pointer map_find(Map map, Pointer key) {
 	MapNode node = map_find_node(map, key);
@@ -220,20 +189,17 @@ DestroyFunc map_set_destroy_value(Map map, DestroyFunc destroy_value) {
 	return old;
 }
 
-// Απελευθέρωση μνήμης που δεσμεύει το map
 void map_destroy(Map map) {
 	for (int i = 0; i < map->capacity; i++) {
-		MapNode /*temp_node,*/node=&map->array[i];
-		if (node->state!=0) {
+		MapNode node=&map->array[i];
+		if (node->state!=0) {  //διασχιζουμε καθε hash το οποιο εχει στοιχεια
 			while (node!=NULL)  {
-				if (map->destroy_key != NULL)
+				if (map->destroy_key != NULL)  //και καλουμε τις destroy
 					map->destroy_key(node->key);
 				if (map->destroy_value != NULL)
 					map->destroy_value(node->value);
 			
-			//temp_node=node;
 			node=node->next;
-			//free(temp_node);
 			}
 		}
 	}
@@ -242,11 +208,9 @@ void map_destroy(Map map) {
 	free(map);
 }
 
-/////////////////////// Διάσχιση του map μέσω κόμβων ///////////////////////////
 
 MapNode map_first(Map map) {
-	//Ξεκινάμε την επανάληψή μας απο το 1ο στοιχείο, μέχρι να βρούμε κάτι όντως τοποθετημένο
-	for (int i = 0; i < map->capacity; i++)  {
+	for (int i = 0; i < map->capacity; i++)  { //ψαχνουμε το πρωτο στοιχειο που να εχει true κατασταση
 		MapNode node=&(map->array[i]) ;
 		if (  node->state != 0)  {
 			return &map->array[i];
@@ -257,13 +221,13 @@ MapNode map_first(Map map) {
 
 MapNode map_next(Map map, MapNode node) {
 	MapNode next=node->next;
-	if (next!=NULL)  {
+	if (next!=NULL)  {  //αν υπαρχει επομενο στοιχειο με ιδιο hash το επιστρεφουμε 
 		return next;
 	}
 	else  {
-		uint pos=(map->hash_function(node->key)+1)%map->capacity;
-		for (int i=1;pos<map->capacity;i++)  {
-			if (map->array[pos].state!=0)  {
+		uint pos=(map->hash_function(node->key)+1)%map->capacity; //αλλιως 
+		for (int i=1;pos<map->capacity;i++)  {  
+			if (map->array[pos].state!=0)  {  //επιστρεφουμε το επομενο hash που εχει στοιχεια
 				return &map->array[pos];
 			}
 			else  {
@@ -271,7 +235,7 @@ MapNode map_next(Map map, MapNode node) {
 			}
 		}
 	}
-	return MAP_EOF;
+	return MAP_EOF; // αν φτασουμε στο τελος του πινακα 
 }
 
 Pointer map_node_key(Map map, MapNode node) {
@@ -283,7 +247,6 @@ Pointer map_node_value(Map map, MapNode node) {
 }
 
 MapNode map_find_node(Map map, Pointer key) {
-	// Διασχίζουμε τον πίνακα, ξεκινώντας από τη θέση που κάνει hash το key, και για όσο δε βρίσκουμε EMPTY
 	uint pos= map->hash_function(key) % map->capacity;
 	MapNode node=&(map->array[pos]);
 	if (node->state==1)  {
@@ -297,7 +260,6 @@ MapNode map_find_node(Map map, Pointer key) {
 	return MAP_EOF;
 }
 
-// Αρχικοποίηση της συνάρτησης κατακερματισμού του συγκεκριμένου map.
 void map_set_hash_function(Map map, HashFunc func) {
 	map->hash_function = func;
 }
